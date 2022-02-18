a82d4eef369d0e227d353f0141318fa9ff7c3006
interesting1113 <sylviayang1113@gmail.com>
2022-02-18 11:16:52 +0900

    http middleware

--- api/defs/errs.go
+++ api/defs/errs.go
@@ -14,4 +14,6 @@ type ErroResponse struct {
 var (
 	ErrorRequestBodyParseFailed = ErroResponse{HttpSC: 400, Error: Err{Error: "Request body is not correct", ErrorCode: "001"}}
 	ErrorNotAuthUser = ErroResponse{HttpSC: 401, Error: Err{Error: "User authentication failed.", ErrorCode: "002"}}
+	ErrorDBError = ErroResponse{HttpSC: 500, Error: Err {Error: "DB ops failed", ErrorCode: "003"}}
+	ErrorInternalFaults = ErroResponse{HttpSC: 500, Error: Err{Error: "Internal service error", ErrorCode: "004"}}
 )
--- api/main.go
+++ api/main.go
@@ -5,6 +5,21 @@ import (
 	"net/http"
 )
 
+type middleWareHandler struct {
+	r *httprouter.Router
+}
+
+func NewMiddleWareHandler(r *httprouter.Router) http.Handler {
+	m := middleWareHandler{}
+	m.r = r
+	return m
+}
+
+func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// check session
+	m.r.ServeHTTP(w, r)
+}
+
 func RegisterHandlers() *httprouter.Router {
 	router := httprouter.New()
 
@@ -17,5 +32,8 @@ func RegisterHandlers() *httprouter.Router {
 
 func main() {
 	r := RegisterHandlers()
-	http.ListenAndServe(":8000", r)
+	mh := NewMiddleWareHandler(r)
+	http.ListenAndServe(":8000", mh)
 }
+
+
