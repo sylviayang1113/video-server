bd5555f4caf82eea1e8f41f4bfbfd0d7f20965ed
interesting1113 <sylviayang1113@gmail.com>
2022-02-21 14:24:44 +0900

    在http middleware中嵌入流控

--- stream_server/main.go
+++ stream_server/main.go
@@ -6,6 +6,18 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type middleWareHandler struct {
+	r *httprouter.httprouter
+	l *ConnLimiter
+}
+
+func NewMiddleWareHandler(r *httprouter.Router, cc int) http.Handler {
+	m := middleWareHandler{}
+	m.r = r
+	m.l = NewConnLimiter()
+	return m
+}
+
 func RegisterHandlers() *httprouter.Router {
 	router := httprouter.New()
 
@@ -16,7 +28,18 @@ func RegisterHandlers() *httprouter.Router {
 	return router
 }
 
+func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !m.l.GetConn() {
+		sendErrorResponse(w, http.StatusTooManyRequests, "Too many requests")
+		return 
+	}
+
+	m.r.ServeHTTP(w, r)
+	defer m.l.ReleaseConn()
+}
+
 func main() {
 	r := RegisterHandlers()
-	http.ListenAndServe(":9000", r)
+	mh := NewMiddleWareHandler(r, 2)
+	http.ListenAndServe(":9000", mh)
 }
