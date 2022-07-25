aaf23ad55523da174f0e59c324d36d9881c067fe
interesting1113 <sylviayang1113@gmail.com>
2022-07-25 19:50:22 +0900

    runner的生产者消费模型实现

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/interesting1113/video-server.git
+
+go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+)
--- scheduler/taskrunner/runner.go
+++ scheduler/taskrunner/runner.go
@@ -25,3 +25,44 @@ func NewRunner(size int, longLived bool, d fn, e fn) *Runner {
 		Executor: e,
 	}
 }
+
+func (r *Runner) startDispatch() {
+	defer func() {
+		if !r.longLived {
+			close(r.Controller)
+			close(r.Data)
+			close(r.Error)
+		}
+	}()
+	for {
+		select {
+			case c :=<- r.Controller:
+				if c == READY_TO_DISPATCH {
+					err := r.Dispatcher(r.Data)
+					if err != nil {
+						r.Error <- CLOSE
+					} else {
+						r.Controller <- READ_TO_EXECUTE
+					}
+				}
+
+				if c == READ_TO_EXECUTE {
+					err := r.Executor(r.Data)
+					if err != nil {}
+					r.Error <- CLOSE
+				} else {
+					r.Controller <- READY_TO_DISPATCH
+				}
+			case e :=<- r.Error:
+				if e == CLOSE {
+					return
+				}
+			default:
+		}
+	}
+}
+
+func (r *Runner) startAll() {
+	r.Controller <- READY_TO_DISPATCH
+	r.startDispatch()
+}
