59d77a1d5b715046cc4f5863e69f371f8026cb43
interesting1113 <sylviayang1113@gmail.com>
2022-08-10 17:25:48 +0900

    静态页面渲染

--- web/handlers.go
+++ web/handlers.go
@@ -1,1 +1,22 @@
-package web
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type HomePage struct {
+	Name string
+}
+func homeHandler(w http.ResponseWriter, r *http.Request, ps *httprouter.Params)  {
+	p := &HomePage{Name: "interesting1113"}
+	t, e := template.ParseFiles("./template/home.html")
+	if e != nil {
+		log.Printf("Parsing template home.html error: %s", e)
+		return
+	}
+
+	t.Execute(w, p)
+}
--- web/main.go
+++ web/main.go
@@ -1,1 +1,35 @@
-package web
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func registerHandler() *httprouter.Router {
+	router := httprouter.New()
+
+	router.GET("/", homeHandler)
+
+	router.POST("/", homeHandler)
+
+	//router.GET("/userhome", userHomeHandler)
+	//
+	//router.POST("/userhome", userHomeHandler)
+	//
+	//router.POST("/api", apiHandler)
+	//
+	//router.GET("/videos/:vid-id", proxyVideoHandler)
+	//
+	//router.POST("/upload/:vid-id", proxyUploadHandler)
+
+	router.ServeFiles("/statics/*filepath", http.Dir("./templates"))
+
+	return router
+
+}
+
+func main()  {
+	r := registerHandler()
+	http.ListenAndServe(":8080", r)
+}
+
