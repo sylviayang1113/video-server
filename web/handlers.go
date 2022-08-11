cc889e0dbcffb6e1e508e472a6949f9f6e2589c8
interesting1113 <sylviayang1113@gmail.com>
2022-08-11 12:12:59 +0900

    home handler

--- web/handlers.go
+++ web/handlers.go
@@ -11,12 +11,21 @@ type HomePage struct {
 	Name string
 }
 func homeHandler(w http.ResponseWriter, r *http.Request, ps *httprouter.Params)  {
-	p := &HomePage{Name: "interesting1113"}
-	t, e := template.ParseFiles("./template/home.html")
-	if e != nil {
-		log.Printf("Parsing template home.html error: %s", e)
+	cname, err1 := r.Cookie("username")
+	sid, err2 := r.Cookie("session")
+	if err1 != nil || err2 != nil {
+		p := &HomePage{Name: "interesting1113"}
+		t, e := template.ParseFiles ("./template/home.html")
+		if e != nil {
+			log.Printf("Parsing template home.html error: %s", e)
+			return
+		}
+		t.Execute(w, p)
 		return
 	}
 
-	t.Execute(w, p)
+	if len(cname.Value) != 0 && len(sid.Value) != 0 {
+		http.Redirect(w, r, "/userhome", http.StatusFound)
+		return
+	}
 }
