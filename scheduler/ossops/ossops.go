6148e642da3c4f529aab7cb8bb90fb7c8b69784d
interesting1113 <sylviayang1113@gmail.com>
2022-08-31 18:51:18 +0900

    UploadToOss

--- scheduler/ossops/ossops.go
+++ scheduler/ossops/ossops.go
@@ -1,6 +1,10 @@
 package ossops
 
-import "github.com/interesting1113/video-server.git/web/config"
+import (
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/interesting1113/video-server.git/web/config"
+	"log"
+)
 
 var EP string
 var AK string
@@ -12,6 +16,26 @@ func init() {
 	EP = config.GetOssAddr()
 }
 
-func UploadToOss(filename string, path string, bn string) bool {}
+func UploadToOss(filename string, path string, bn string) bool {
+	client, err := oss.New(EP, AK, SK)
+	if err != nil {
+		log.Printf("Init oss service error: %s", err)
+		return false
+	}
+
+	bucket, err := client.Bucket(bn)
+	if err != nil {
+		log.Printf("Getting bucket error: %s", err)
+		return false
+	}
+
+	err = bucket.UploadFile(filename, path, 500*1024, oss.Routines(3))
+	if err != nil {
+		log.Printf("Uploading object error: %s", err)
+		return false
+	}
+
+	return true
+}
 
 func DeleteObject(filename string, bn string) bool {}
