c186f2aecf622606a8f80988e5fd3e05a7d1d530
interesting1113 <sylviayang1113@gmail.com>
2022-09-01 19:49:38 +0900

    delete object api

--- scheduler/ossops/ossops.go
+++ scheduler/ossops/ossops.go
@@ -38,4 +38,24 @@ func UploadToOss(filename string, path string, bn string) bool {
 	return true
 }
 
-func DeleteObject(filename string, bn string) bool {}
+func DeleteObject(filename string, bn string) bool {
+	client, err := oss.New(EP, AK, SK)
+	if err != nil {
+		log.Printf("Init oss service error: %s", err)
+		return false
+	}
+
+	bucket, err := client.Bucket(bn)
+	if err != nil {
+		log.Printf("Getting bueckt error: %s", err)
+		return false
+	}
+
+	err = bucket.DeleteObject(filename)
+	if err != nil {
+		log.Printf("Deleting object error: %s", err)
+		return false
+	}
+
+	return true
+}
